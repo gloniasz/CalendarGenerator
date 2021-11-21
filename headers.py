d23edc575438823bc578518aa93e4acf33c6bcca
Glonek,  Mariusz <mariusz.glonek@mobica.com>
2021-11-21 13:14:27 +0100

    Addition of an engine responsible for downloading public holidays' list. + changes in constants utilizing API addresses + minor changes to headers.py file + addition of holidays_downloader.py file responsible for communication   with holiday API (https://date.nager.at/swagger/index.html); available   of downloading country list and holidays list for a year and country   (plus filter it by county)

--- headers.py
+++ headers.py
@@ -3,17 +3,23 @@ from month_data import month_basics
 
 class header_generator:
     headers : int = []
-    def __init__(self, month_data: month_basics) -> None:
-        for day_nr in range(month_data.days_amount):
-            self.headers.append(day_nr % 7 + month_data.day_first)
+    def setHeaders(self, days_amount: int, day_first: int):
+        for day_nr in range(days_amount):
+            self.headers.append(day_nr % 7 + day_first)
+
+    def __init__(self, month_data: month_basics =  None) -> None:
+        if month_basics is None:
+            self.setHeaders(month_data.days_amount, month_data.day_first)
+    def __init__(self) -> None:
         pass
 
     def daysNamesHeader(self, abbr : bool =  False):
+        day_names : int = []
+        headers_days : int = []
         if abbr:
             day_names = constants.DAYS_ABBR
         else:
             day_names = constants.DAYS
-        headers_days = []
         for day_nr in range(len(self.headers)):
             headers_days.append(day_names[self.headers[day_nr]])
         return headers_days
@@ -22,7 +28,8 @@ class header_generator:
         return list(range(1, len(self.headers)+1))
 
 mb = month_basics(11, 2021)
-headers = header_generator(mb)
+headers = header_generator()
+headers.setHeaders(30,0)
 
 print(headers.daysNamesHeader(True))
 print(headers.daysNumbersHeader())
---
+++ holidays_downloader.py
@@ -0,0 +1,49 @@
+from typing import Dict
+import constants
+import urllib.request, json
+
+# using API provided by: https://date.nager.at/swagger/index.html
+
+class holidayDonwloader:
+    address : str = ""
+    countries_endpoint : str = ""
+    holidays_endpoint : str = "" 
+    def __init__(self, address : str = constants.HOLIDAY_API_ADDRESS, countries : str = constants.COUNTRIES_LIST_ENDPOINT, holidays : str = constants.PUBLIC_HOLIDAYS_ENDPOINT) -> None:
+        self.setApiAddress(address)
+        self.setCountriesListEndpoint(countries)
+        self.setHolidaysListEndpoint(holidays)
+    
+    def setApiAddress(self, address : str):
+        self.address = address
+    
+    def setCountriesListEndpoint(self, countries : str):
+        self.countries_endpoint = countries
+    
+    def setHolidaysListEndpoint(self, holidays : str):
+        self.holidays_endpoint = holidays
+
+    def getCountriesDict(self) -> Dict[str, str]:
+        countries = {}
+        with urllib.request.urlopen(self.address+self.countries_endpoint) as url:
+            jsonData = json.loads(url.read().decode())
+            for record in jsonData:
+                countries[record['name']] = record['countryCode']
+        return countries
+
+    def getPublicHolidaysDatesList(self, year, country : str, county : str = ""):
+        dates = []
+        with urllib.request.urlopen(self.address+self.createHolidayEndpoint(year, country)) as url:
+            jsonData = json.loads(url.read().decode())
+            for record in jsonData:
+                if county == "":
+                    dates.append(record['date'])
+                elif record['counties'] == None or county in record['counties']:
+                    dates.append(record['date'])
+        return dates
+    
+    def createHolidayEndpoint(self, year, country : str):
+        return self.holidays_endpoint.replace("_",  str(year), 1).replace("_", country, 1)
+
+hd = holidayDonwloader()
+# print(hd.getCountriesDict())
+print(hd.getPublicHolidaysDatesList("2021", "DE"))
